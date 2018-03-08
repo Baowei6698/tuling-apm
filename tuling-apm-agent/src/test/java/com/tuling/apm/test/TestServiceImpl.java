3f7168d31e25c2d39d883eb4154303c1ee1f04eb
Tommy <276386551@qq.com>
2018-03-08 18:25:23 +0800

    apm-agent 框架编写，实现service接口采集

---
+++ tuling-apm-agent/src/test/java/com/tuling/apm/test/TestServiceImpl.java
@@ -0,0 +1,10 @@
+package com.tuling.apm.test;
+
+/**
+ * Created by Tommy on 2018/3/8.
+ */
+public class TestServiceImpl {
+    public void getUser(String userid) {
+        System.out.println("获取用户信息:" + userid);
+    }
+}
